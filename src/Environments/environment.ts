5ccc884fc7a2009ea53e464f32ad13e97af75c6b
Gaurav Kumar Dixit <dixitgauravkumar69@gmail.com>
2026-03-13 18:25:24 +0530

    Url change hua hai..............

--- src/Environments/environment.ts
+++ src/Environments/environment.ts
@@ -1,5 +1,5 @@
 // export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL='https://compilerbackend-kg3b.onrender.com';
+export const BASE_URL='https://compilerbackend2-kg3b.onrender.com';
 
 export const environment = {
   production: false

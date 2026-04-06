ae7b5feff5c82ff256f5380f190d540440ae4eef
Gaurav Kumar Dixit <dixitgauravkumar69@gmail.com>
2026-04-06 16:21:17 +0530

    403 page and url

--- src/app/Components/access-denied/access-denied.ts
+++ src/app/Components/access-denied/access-denied.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-access-denied',
-  imports: [RouterLink],
+  imports: [RouterLink
+  ],
   templateUrl: './access-denied.html',
   styleUrl: './access-denied.css',
 })
-export class AccessDenied {}
+export class AccessDenied {
+  constructor(private router:Router)
+  {}
+
+  goHome() {
+  this.router.navigate(['/']);
+}
+
+goBack() {
+  window.history.back();
+}
+}

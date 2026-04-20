29cb3b19ed79fde1f35c9c58902839486923e98c
Gaurav Kumar Dixit <dixitgauravkumar69@gmail.com>
2026-04-20 18:29:30 +0530

    Change url

--- src/Environments/environment.ts
+++ src/Environments/environment.ts
@@ -1,8 +1,13 @@
-export const BASE_URL = 'http://localhost:8080';
+// export const BASE_URL = 'http://localhost:8080';
 
 // export const BASE_URL='https://compilerbackend2.onrender.com';
+
+
+const production=true;
+
+export const BASE_URL = production ? 'https://complexitycalculation.onrender.com' : 'http://localhost:8080';
 export const BASE_URL_CCOMPLEXITY='https://complexitycalculation.onrender.com';
 
 export const environment = {
-  production: false
+ 
 };
